0a2526b98f37337598354c6b6184df9f729a7d3a
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-08 10:32:27 +0100

    update connection.py

--- db/connection.py
+++ db/connection.py
@@ -9,7 +9,6 @@ load_dotenv(override=True)
 def connect_to_db():
     return pg8000.native.Connection(
         user=os.getenv("PG_USER"), 
-        password=os.getenv("PG_PASSWORD"),
         database=os.getenv("PG_DATABASE"),
         host=os.getenv("PG_HOST"),
         port=int(os.getenv("PG_PORT"))
