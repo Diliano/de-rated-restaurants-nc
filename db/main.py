3bbfa0395a228d6c6abe1503867fd9eabbcfca71
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-08 11:31:35 +0100

    build functionality of the POST /restaurants endpoint

--- db/main.py
+++ db/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pg8000.native import literal
 from connection import connect_to_db, close_db_connection
 
 
@@ -6,15 +8,39 @@ app = FastAPI()
 
 
 @app.get("/")
-def get_root():
+def read_root():
     return {"message": "all ok"}
 
 
 @app.get("/restaurants")
-def get_restaurants():
+def read_restaurants():
     conn = connect_to_db()
     restaurants_data = conn.run("""SELECT * FROM restaurants;""")
     column_names = [c["name"] for c in conn.columns]
     formatted_restaurants_data = [dict(zip(column_names, restaurant)) for restaurant in restaurants_data]
     close_db_connection(conn)
     return {"restaurants": formatted_restaurants_data}
+
+
+class NewRestaurant(BaseModel):
+    restaurant_name: str
+    area_id: int
+    cuisine: str
+    website: str
+
+@app.post("/restaurants")
+def add_new_restaurant(new_restaurant: NewRestaurant):
+    conn = connect_to_db()
+    insert_query = f"""
+        INSERT INTO restaurants
+            (restaurant_name, area_id, cuisine, website)
+        VALUES
+            ({literal(new_restaurant.restaurant_name)}, {literal(new_restaurant.area_id)}, {literal(new_restaurant.cuisine)}, {literal(new_restaurant.website)})
+        RETURNING restaurant_id;
+    """
+    restaurant_id = conn.run(sql=insert_query)[0][0]
+    restaurant_data = conn.run(f"""SELECT * FROM restaurants WHERE restaurant_id = {literal(restaurant_id)}""")[0]
+    column_names = [c["name"] for c in conn.columns]
+    formatted_restaurant_data = dict(zip(column_names, restaurant_data))
+    close_db_connection(conn)
+    return {"restaurant": formatted_restaurant_data}
