05aeba11e1e9d35942e2b9237efddd6bbe0e30be
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-08 11:04:09 +0100

    build functionality for the GET /restaurants endpoint

--- db/main.py
+++ db/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from connection import connect_to_db, close_db_connection
 
 
 app = FastAPI()
@@ -7,3 +8,13 @@ app = FastAPI()
 @app.get("/")
 def get_root():
     return {"message": "all ok"}
+
+
+@app.get("/restaurants")
+def get_restaurants():
+    conn = connect_to_db()
+    restaurants_data = conn.run("""SELECT * FROM restaurants;""")
+    column_names = [c["name"] for c in conn.columns]
+    formatted_restaurants_data = [dict(zip(column_names, restaurant)) for restaurant in restaurants_data]
+    close_db_connection(conn)
+    return {"restaurants": formatted_restaurants_data}
